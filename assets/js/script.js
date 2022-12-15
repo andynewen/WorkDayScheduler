757e3aeb97787a5bdc7f04b11ccd853a3948d215
andynewen <117054251+andynewen@users.noreply.github.com>
2022-12-15 15:09:52 +1100

    added comments to my js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-  
+  // The program should display the hours of the day.
   hours = ["9am", "10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
 
-  
+  // The current date should be printed.
   var today = dayjs().format("MMMM Do YYYY");
   $("#currentDay").append(today);
 
-  
+  // The program should track the current hour in military time.
   var time = dayjs().format("H");
-      
+      // The current hour is logged for reference.
       console.log(time);
-  
+  // The time is converted into an integer.
   var timeInt = parseInt(time) -9;
 
-  
+  // Time blocks are added to the container.
   function timeBlocks() {
-      
+      // A time block is added for each hour in the hours array.
       for (let i = 0; i < 9; i++) {
 
           var timeBlock = $("<div>");
@@ -31,7 +31,7 @@ $(document).ready(function() {
           timeBlock.append(textArea);
           textArea.attr("id","text" + i);
 
-         
+         // The text area is color-coded based on the current time using the specified CSS class.
           if (i < timeInt) { 
           textArea.attr("class", "description past");
           } else if (i > timeInt) {
@@ -48,12 +48,12 @@ $(document).ready(function() {
   }
   timeBlocks();
   
- 
+ // Any saved text is retrieved from local storage.
   for (let i = 0; i < 8; i++) {
       var store = localStorage.getItem("save" + i);
       $("#text" + i).text(store);
   }
-
+// When the save button is clicked, the text is saved to local storage.
   $(".saveBtn").on("click",function(event) {
   var element = event.target.id;
   var match = element.charAt(element.length -1);
